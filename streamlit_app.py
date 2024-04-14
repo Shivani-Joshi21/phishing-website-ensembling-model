987ec4873735192fc9ab052890b23a4dab734b0c
Shivani-Joshi21 <144917476+Shivani-Joshi21@users.noreply.github.com>
2024-04-14 15:58:00 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,134 +1,57 @@
-import pickle
 import streamlit as st
-from sklearn.ensemble import RandomForestClassifier
-
-# loading the trained model
-pickle_in = open('phishing_classifier.pkl', 'rb')
-classifier = pickle.load(pickle_in)
-
-#@st.cache()
-@st.cache_data()
-# defining the function which will make the prediction using the data which the user inputs
-def prediction(index,having_IPhaving_IP_Address,URLURL_Length,Shortining_Service,having_At_Symbol,double_slash_redirecting,Prefix_Suffix,having_Sub_Domain,SSLfinal_State,Domain_registeration_length,popUpWidnow,Iframe,age_of_domain,DNSRecord,web_traffic,Page_Rank,Google_Index,Links_pointing_to_page,Statistical_report):
-    
-    # Making predictions
-    prediction = classifier.predict(
-        [[index,having_IPhaving_IP_Address,URLURL_Length,Shortining_Service,having_At_Symbol,double_slash_redirecting,Prefix_Suffix,having_Sub_Domain,SSLfinal_State,Domain_registeration_length,popUpWidnow,Iframe,age_of_domain,DNSRecord,web_traffic,Page_Rank,Google_Index,Links_pointing_to_page,Statistical_report]])
-
-    if prediction == 1:
-        pred = 'Phishing'
-    else:
-        pred = 'Legitimate'
-    return pred
-
-# this is the main function in which we define our webpage
-def main():
-    # front end elements of the web page
-    html_temp = """
-    <div style ="background-color:yellow;padding:13px">
-    <h1 style ="color:black;text-align:center;">Streamlit Phishing Domain Detection ML App</h1>
-    </div>
-    """
-
-    # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True)
-    st.title('Pragyan AI Case Study @ Fuel')
-    st.image("""https://altcont.fibe.in/wp-content/uploads/2020/06/rendered.png""")
-    st.header('Enter the URL Details for Phishing Detection:')
-    # following lines create boxes in which user can enter data required to make prediction
-    index = st.number_input("Enter Index:")
-    having_IPhaving_IP_Address = st.number_input("Enter having_IPhaving_IP_Address:")
-    URLURL_Length = st.number_input("Enter URLURL_Length:")
-    Shortining_Service = st.number_input("Enter Shortining_Service:")
-    having_At_Symbol = st.number_input("Enter having_At_Symbol:")
-    double_slash_redirecting = st.number_input("Enter double_slash_redirecting:")
-    Prefix_Suffix = st.number_input("Enter Prefix_Suffix:")
-    having_Sub_Domain = st.number_input("Enter having_Sub_Domain:")
-    SSLfinal_State = st.number_input("Enter SSLfinal_State:")
-    Domain_registeration_length = st.number_input("Enter Domain_registeration_length:")
-    popUpWidnow = st.number_input("Enter popUpWidnow:")
-    Iframe = st.number_input("Enter Iframe:")
-    age_of_domain = st.number_input("Enter age_of_domain:")
-    DNSRecord = st.number_input("Enter DNSRecord:")
-    web_traffic = st.number_input("Enter web_traffic:")
-    Page_Rank = st.number_input("Enter Page_Rank:")
-    Google_Index = st.number_input("Enter Google_Index:")
-    Links_pointing_to_page = st.number_input("Enter Links_pointing_to_page:")
-    Statistical_report = st.number_input("Enter Statistical_report:")
-
-    result = ""
-
-    # when 'Predict' is clicked, make the prediction and store it
-    if st.button("Predict"):
-        result = prediction(index,having_IPhaving_IP_Address,URLURL_Length,Shortining_Service,having_At_Symbol,double_slash_redirecting,Prefix_Suffix,having_Sub_Domain,SSLfinal_State,Domain_registeration_length,popUpWidnow,Iframe,age_of_domain,DNSRecord,web_traffic,Page_Rank,Google_Index,Links_pointing_to_page,Statistical_report)
-        st.success('The URL is {}'.format(result))
-
-if _name=='main_':
-    main()
 import pickle
-import streamlit as st
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from extract_features import ExtractFeatures
 
-# loading the trained model
-pickle_in = open('phishing_classifier.pkl', 'rb')
-classifier = pickle.load(pickle_in)
-
-#@st.cache()
-@st.cache_data()
-# defining the function which will make the prediction using the data which the user inputs
-def prediction(index,having_IPhaving_IP_Address,URLURL_Length,Shortining_Service,having_At_Symbol,double_slash_redirecting,Prefix_Suffix,having_Sub_Domain,SSLfinal_State,Domain_registeration_length,popUpWidnow,Iframe,age_of_domain,DNSRecord,web_traffic,Page_Rank,Google_Index,Links_pointing_to_page,Statistical_report):
-    
-    # Making predictions
-    prediction = classifier.predict(
-        [[index,having_IPhaving_IP_Address,URLURL_Length,Shortining_Service,having_At_Symbol,double_slash_redirecting,Prefix_Suffix,having_Sub_Domain,SSLfinal_State,Domain_registeration_length,popUpWidnow,Iframe,age_of_domain,DNSRecord,web_traffic,Page_Rank,Google_Index,Links_pointing_to_page,Statistical_report]])
-
-    if prediction == 1:
-        pred = 'Phishing'
-    else:
-        pred = 'Legitimate'
-    return pred
-
-# this is the main function in which we define our webpage
-def main():
-    # front end elements of the web page
-    html_temp = """
-    <div style ="background-color:yellow;padding:13px">
-    <h1 style ="color:black;text-align:center;">Streamlit Phishing Domain Detection ML App</h1>
-    </div>
+@st.cache_resource
+def get_model():
     """
+    Loads the phishing URL detection model from a pickle file.
+    This function reads and loads a pickled file containing the classifier.
+    Returns:
+        object: The loaded phishing URL detection model.
+    Note:
+        The model should be saved in a file named 'phishing_url_detector.pkl'.
+        XGBoost module must be installed before using the file.
+    """
+    with open('phishing_url_detector.pkl', 'rb') as pickle_model:
+        phishing_url_detector = pickle.load(pickle_model)
+    return phishing_url_detector
 
-    # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True)
-    st.title('Pragyan AI Case Study @ Fuel')
-    st.image("""https://altcont.fibe.in/wp-content/uploads/2020/06/rendered.png""")
-    st.header('Enter the URL Details for Phishing Detection:')
-    # following lines create boxes in which user can enter data required to make prediction
-    index = st.number_input("Enter Index:")
-    having_IPhaving_IP_Address = st.number_input("Enter having_IPhaving_IP_Address:")
-    URLURL_Length = st.number_input("Enter URLURL_Length:")
-    Shortining_Service = st.number_input("Enter Shortining_Service:")
-    having_At_Symbol = st.number_input("Enter having_At_Symbol:")
-    double_slash_redirecting = st.number_input("Enter double_slash_redirecting:")
-    Prefix_Suffix = st.number_input("Enter Prefix_Suffix:")
-    having_Sub_Domain = st.number_input("Enter having_Sub_Domain:")
-    SSLfinal_State = st.number_input("Enter SSLfinal_State:")
-    Domain_registeration_length = st.number_input("Enter Domain_registeration_length:")
-    popUpWidnow = st.number_input("Enter popUpWidnow:")
-    Iframe = st.number_input("Enter Iframe:")
-    age_of_domain = st.number_input("Enter age_of_domain:")
-    DNSRecord = st.number_input("Enter DNSRecord:")
-    web_traffic = st.number_input("Enter web_traffic:")
-    Page_Rank = st.number_input("Enter Page_Rank:")
-    Google_Index = st.number_input("Enter Google_Index:")
-    Links_pointing_to_page = st.number_input("Enter Links_pointing_to_page:")
-    Statistical_report = st.number_input("Enter Statistical_report:")
+st.title("Phishing Website Detector")
+st.header("Are you sure your 'bank' sent that link?")
 
-    result = ""
+# Takes in user input
+input_url = st.text_area("Put in your sus site link here: ")
 
-    # when 'Predict' is clicked, make the prediction and store it
-    if st.button("Predict"):
-        result = prediction(index,having_IPhaving_IP_Address,URLURL_Length,Shortining_Service,having_At_Symbol,double_slash_redirecting,Prefix_Suffix,having_Sub_Domain,SSLfinal_State,Domain_registeration_length,popUpWidnow,Iframe,age_of_domain,DNSRecord,web_traffic,Page_Rank,Google_Index,Links_pointing_to_page,Statistical_report)
-        st.success('The URL is {}'.format(result))
+if input_url != "":
+    
+    # Extracts features from the URL and converts it into a dataframe
+    features_url = ExtractFeatures().url_to_features(url=input_url)
+    features_dataframe = pd.DataFrame.from_dict([features_url])
+    features_dataframe = features_dataframe.fillna(-1)
+    features_dataframe = features_dataframe.astype(int)
 
-if _name=='main_':
-    main()
+    st.write("Okay!")
+    st.cache_data.clear()
+    prediction_str = ""
+
+    # Predict outcome using extracted features
+    try: 
+        phishing_url_detector = get_model()
+        prediction = phishing_url_detector.predict(features_dataframe)
+        if prediction == int(True):
+            prediction_str = 'Phishing Website. Do not click!'
+        elif prediction == int(False):
+            prediction_str = 'Not Phishing Website, stay safe!'
+        else:
+            prediction_str = ''
+        st.write(prediction_str)
+        st.write(features_dataframe)
+
+    except Exception as e:
+        print(e)
+        st.error("Not sure, what went wrong. We'll get back to you shortly!")
+
+else:
+    st.write("")
